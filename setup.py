14e06ec1409fee0fdb4bcdbc33eb608db6ece08c
Peter Causey-Freeman <pjf9@leicester.ac.uk>
2019-08-19 15:32:54 +0100

    sort setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,8 @@ setup(license="Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)",
       install_requires=[
           "attrs>=17.4.0",  # https://github.com/biocommons/hgvs/issues/473
           "biocommons.seqrepo",
-          "biopython==1.69",    # 1.70 fails on rtd due to numpy absence
+          "numpy",
+          "biopython==1.74",    # 1.70 fails on rtd due to numpy absence
           "bioutils>=0.2.2",
           "configparser>=3.3.0",
           "enum34",
